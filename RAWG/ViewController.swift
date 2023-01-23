06017ec15a6460d38b9878e57c34dddfe882e704
Zeynep Özdemir Açıkgöz <zeynmir@gmail.com>
2023-01-23 11:22:21 +0300

    Initial Commit

---
+++ RAWG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAWG
+//
+//  Created by Zeynep Özdemir Açıkgöz on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
